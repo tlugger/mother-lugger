6c062a434aab8af6363e51d8c9461907269fbba7
Tyler Lugger <notnottyler@gmail.com>
2019-04-13 23:38:22 -0600

    sort styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { ImageBar,
          Libby,
+         Mentions,
+         Overview,
          Title } from '../../components';
 import './App.scss';
 
@@ -18,8 +20,7 @@ class App extends Component {
             </p>
           </div>
           <div className="App-section">
-            <h1>My Mother</h1>
-            <p className="App-par">From humble beginnings to the top of her class in motherhood</p>
+            <Overview/>
           </div>
           <div className="App-section">
             <h1>Introducing Tyler</h1>
@@ -35,10 +36,7 @@ class App extends Component {
             <h5>pics of Justice, Libby, Zeus, and of course Luna</h5>
           </div>
           <div className="App-section">
-            <h1>Honorable Motherhood Mentions</h1>
-            <p className="App-par">My Grandma: The mother of all mothers</p>
-            <p className="App-par">My Aunt: My mom away from mom</p>
-            <p className="App-par">My Girlfriends Mom: A mother not unlike my own.</p>
+            <Mentions/>
           </div>
           <div className="App-section">
             <Libby/>
