6181e4a6a92e6210576cb1cfaf39bdce6301df1a
Tyler Lugger <notnottyler@gmail.com>
2019-04-13 23:37:43 -0600

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-export { default as Title } from './Title/Title';
 export { default as App } from './App/App';
-export { default as Libby } from './Libby/Libby';
 export { default as ImageBar } from './ImageBar/ImageBar';
+export { default as Libby } from './Libby/Libby';
+export { default as Mentions } from './Mentions/Mentions';
+export { default as Overview } from './Overview/Overview';
+export { default as Title } from './Title/Title';
